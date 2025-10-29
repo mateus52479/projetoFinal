bcd79307a63bfaace36e1932b120e1d3c2108e53
Mateus De Boit Dos Santos <mateus.248123@alunosatc.edu.br>
2025-10-28 21:05:03 -0300

    movimentação e animações de ataques

--- Assets/Player/Script/Player.cs
+++ Assets/Player/Script/Player.cs
@@ -1,58 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
-    private Rigidbody2D rigid;
-
-    void Start()
-    {
-        rigid = GetComponent<Rigidbody2D>();
-    }
+   public float speed;
+   public Rigidbody2D rb;
+   public Vector2 mov;
+   public Animator anim;
 
     void Update()
     {
-        move();
+        mov.x = Input.GetAxisRaw("Horizontal");
+        mov.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("Horizontal", mov.x);
+        anim.SetFloat("Vertical", mov.y);
+        anim.SetFloat("Speed", mov.sqrMagnitude);
+
+        mov.Normalize();
+
     }
 
-    void move()
+    private void FixedUpdate()
     {
-        float teclas_horizontais = Input.GetAxis("Horizontal");
-        rigid.linearVelocity = new Vector2(teclas_horizontais * speed, rigid.linearVelocity.y);
-
-        float teclas_verticais = Input.GetAxis("Vertical");
-        rigid.linearVelocity = new Vector2(rigid.linearVelocity.x, teclas_verticais * speed);
-
-         if (teclas_horizontais > 0)
-        {
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-
-        if (teclas_horizontais < 0)
-        {
-            transform.eulerAngles = new Vector2(0, 180);
-        }
-
-        if (teclas_horizontais == 0)
-        {
-        }
-        
-
-
-
-        if (teclas_verticais > 0)
-        {
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-
-        if (teclas_verticais < 0)
-        {
-            transform.eulerAngles = new Vector2(0, 180);
-        }
-
-        if (teclas_verticais == 0 )
-        {
-        }
-
+        rb.MovePosition(rb.position + mov * speed * Time.fixedDeltaTime);
     }
 }
