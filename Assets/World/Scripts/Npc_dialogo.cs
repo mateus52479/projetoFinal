398d0346e9de0bb77a3b00b1b08f0c4a9b04f4ac
pauloarturtournier <paulotournier1234@gmail.com>
2025-11-14 17:22:09 -0300

    Revert "Menu e pause"
    This reverts commit 18dc57efbc422b74b7dc87192e4963d530eba328.

--- Assets/World/Scripts/Npc_dialogo.cs
+++ Assets/World/Scripts/Npc_dialogo.cs
@@ -19,7 +19,7 @@ public class Npc_dialogo : MonoBehaviour
 
     public void Interact()
     {
-        Collider2D hit = Physics2D.OverlapCircle(transform.position,  playerLayer);
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, radius, playerLayer);
 
         if (hit != null) 
         { 
